3111ae1309c67ab9dad0d38d964c732af382340c
TrinhHuynh2801 <trinhhuynhthien2801@gmail.com>
2024-06-12 15:20:24 +0700

    init users route

---
+++ src/controllers/users.controllers.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from 'express'
+
+export const loginController = (req: Request, res: Response) => {
+  console.log(req.body)
+  res.json({
+    message: 'Login Success'
+  })
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-const name: string = 'Dư Thanh Được'
-console.log(name)
+import express from 'express'
+import usersRouter from './routes/users.routes'
+const app = express()
+const port = 3000
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.use(express.json())
+app.use('/users', usersRouter)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
