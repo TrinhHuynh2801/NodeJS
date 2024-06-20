1413b90c1423a1a03f59539248df0b2a5304b185
TrinhHuynh2801 <trinhhuynhthien2801@gmail.com>
2024-06-20 18:43:08 +0700

    validate register body

---
+++ src/utils/valiation.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+import { ContextRunner } from 'express-validator'
+
+// can be reused by many routes
+const validate = (validations: ContextRunner[]) => {
+  return async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    // sequential processing, stops running validations chain if one fails.
+    for (const validation of validations) {
+      const result = await validation.run(req)
+      console.log(result)
+      if (!result.isEmpty()) {
+        return res.status(400).json({
+          errors: result.mapped()
+        })
+      }
+    }
+
+    next()
+  }
+}
+export default validate
